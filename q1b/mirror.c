98ba474e214b457ac4c5963105691b0bba29ac9a
titus njiru <njirutitus@gmail.com>
2021-03-25 08:48:32 +0300

    Implementing q1b/mirror.c

--- q1b/mirror.c
+++ q1b/mirror.c
@@ -12,4 +12,22 @@
 // Name: ERROR_NO_NAME 
 // login ID: ERROR_NO_LOGIN 
 ///////////////////////////////////////////////////////////////////////////// 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mirror.h"
 
+int main() {
+    char *m = mirror("Hello");
+    printf("%s",m);
+    free(m);
+}
+char *mirror(const char *str) {
+    int len = strlen(str);
+    char *s;
+    strcpy(s,str);
+    char *m = malloc(len*2*sizeof(char));
+    strcpy(m,s);
+    strcat(m,strrev(s));
+    return m;
+}
