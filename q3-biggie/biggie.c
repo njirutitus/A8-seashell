6308657fcdcb31466a9e5dba6d756d31099f4e3e
titus njiru <njirutitus@gmail.com>
2021-03-25 22:04:26 +0300

    biggie_mult implementation not tested

--- q3-biggie/biggie.c
+++ q3-biggie/biggie.c
@@ -18,10 +18,12 @@
 #include <strings.h>
 
 int main() {
-  struct biggie *big = biggie_create("986321");
-  struct biggie *big1 = biggie_create("344321");
+  struct biggie *big = biggie_create("123");
+  struct biggie *big1 = biggie_create("123");
   biggie_add(big,big1);
   biggie_print(big,true);
+  // biggie_mult(big,big1);
+  // biggie_print(big,true);
   biggie_destroy(big);
   biggie_destroy(big1);
   return 0;
@@ -86,7 +88,9 @@ struct biggie *biggie_create(const char *s) {
 }
 
 void biggie_destroy(struct biggie *big) {
+  big->digits = NULL;
   free(big->digits);
+  big = NULL;
   free(big);
 }
 
@@ -110,8 +114,6 @@ void biggie_add(struct biggie *n, const struct biggie *m) {
   int carry = 0;
   int sum = 0;
   int digit;
-  
-  if (max_length > lenn) n->digits = realloc(n->digits, max_length * sizeof(char *));
 
   for ( int i = 0 ; i < max_length ; i++) {
     if ( lenn >= i && lenm >= i)
@@ -122,21 +124,105 @@ void biggie_add(struct biggie *n, const struct biggie *m) {
        sum = n->digits[i] - '0';
     sum = sum + carry;
     digit = sum % 10;
-    carry = ( sum - carry ) / 10;
+    carry = ( sum - digit ) / 10;
     n->digits[i] = digit + '0';
   }
 
   if (carry > 0) {
+    n->digits = realloc(n->digits, max_length * sizeof(char *));
     n->digits[max_length] = carry + '0';
   }
 
 }
 
 void biggie_sub(struct biggie *n, const struct biggie *m) {
+  int lenn = strlen(n->digits);
+  int lenm = strlen(m->digits);
+  int max_length = lenn > lenm ? lenn : lenm;
+  int borrow = 0;
+  int diff = 0;
+  int digit;
+  int num1, num2;
+  
+  if (max_length > lenn) n->digits = realloc(n->digits, max_length * sizeof(char *));
+
+  for ( int i = 0 ; i < max_length ; i++) {
+    if ( lenn >= i && lenm >= i) {
+      num1 = (n->digits[i] - '0');
+      num2 = (m->digits[i] - '0');
+      if(num1 > num2) {
+        borrow = 1;
+        diff = 10 + num1 - num2;
+      }
+      else {
+        diff = num1 - num2;
+      }
+    }
+
+    else if (lenm >= i)  {
+      num2 =  (m->digits[i] - '0');
+    }
+    else {
+      num1 = n->digits[i] - '0';
+      diff = num1;
+
+    }
+  }
   
 }
 
 void biggie_mult(struct biggie *n, const struct biggie *m) {
+
+  // string lengths
+  int len1 = strlen(n->digits);
+  int len2 = strlen(m->digits);
+  
+  // positions
+  int i_n1 = 0;
+  int i_n2 = 0;
+
+  for (int i = 0; i < len1; i++)  {
+    int carry = 0;
+    int n1 = n->digits[i] - '0';
+
+    // multiply with every digit in string m
+    i_n2 = 0;
+    for (int j = 0; j < len1; j++)  {
+      // Take current digit of m
+      int n2 = m->digits[j] - '0';
+      // Multiply with current digit of first number
+      // and add result to previously stored result
+      // at current position. 
+      int sum = n1*n2 + (n->digits[i_n1 + i_n2] - '0') + carry;
+    
+      // Carry for next iteration
+      carry = sum/10;
+      // Store result
+      n->digits[i_n1 + i_n2] = (sum % 10 + '0');
+
+      i_n2++;
+    }
+    // store carry in next cell
+    if (carry > 0)
+      n->digits[i_n1 + i_n2] += carry + '0';
+
+      // To shift position to left after every
+      // multiplication of a digit in num1.
+      i_n1++;
+
+  }
+
+  // ignore '0's from the right
+  int i = strlen(n->digits) - 1;
+  while (i>=0 && (n->digits[i] - '0') == 0)
+    i--;
+
+  // If all were '0's - means either both or
+  // one of n or m were '0'
+  if (i == -1) {
+   n->digits = realloc(n->digits,sizeof(char *));
+   n->digits[0] = '0';
+  }
 }
 
 bool biggie_eq(const struct biggie *n, const struct biggie *m) {
